964e69a5681ece77ece8d3c770bf2a66f221070c
Michael <michaelanguay@outlook.com>
2017-04-01 12:06:14 -1000

    cleaned up code and made it better

--- bowl.js
+++ bowl.js
@@ -1,26 +1,28 @@
 var bowling = function(frames) {
 
   var score = 0;
+  function lastFrame() {
+    score += frames[9].ball1 + frames[9].ball2 + frames[9].ball3;
+
+  }
 
   for(var i = 0;i < frames.length; i++) {
     //for last frame
-    if(frames[9].ball1 && frames[9].ball2 >= 10) {
-      score += frames[9].ball1 + frames[9].ball2 + frames[9].ball3;
-      return score;
+    if(frames[9].ball1 === 10 && i === 9) {
+      lastFrame();
+      break;
     }
 
     // for strikes
     if(frames[i].ball1 === 10) {
-      score += frames[i].ball1 + ((frames[i + 1].ball1 + frames[i + 1].ball2));
-      return score ;
+      score += frames[i].ball1 + frames[i + 1].ball1 + frames[i + 1].ball2;
     }
     // for spares
     if(frames[i].ball1 !== 10 && frames[i].ball1 + frames[i].ball2 === 10) {
       score += frames[i].ball1 + frames[i].ball2 + frames[i + 1].ball1;
-      return score;
     }
     //for anything other than spares and strikes
-     if (frames[i].ball1 + frames[i].ball2 < 10) {
+    if (frames[i].ball1 + frames[i].ball2 < 10) {
       score += frames[i].ball1 + frames[i].ball2;
     }
   }
--- tests/bowl.spec.js
+++ tests/bowl.spec.js
@@ -10,13 +10,14 @@ describe('bowling',() => {
   it('should return total', () => {
     expect(bowling([{ball1: 0 ,ball2: 0},{ball1: 0 ,ball2: 0},{ball1: 0 ,ball2: 0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0}])).to.equal(0);
     expect(bowling([{ball1: 3 ,ball2:  3 },{ball1: 2 ,ball2: 2},{ball1: 0 ,ball2: 0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0}])).to.equal(10);
+    expect(bowling([{ball1: 10 ,ball2:  0 },{ball1: 1 ,ball2: 1},{ball1: 1 ,ball2: 1},{ball1: 1 ,ball2:1},{ball1: 1 ,ball2:1},{ball1: 1 ,ball2:1},{ball1: 1 ,ball2:1},{ball1: 1 ,ball2:1},{ball1: 1 ,ball2:1},{ball1: 1 ,ball2:1}])).to.equal(30);
 
   });
   it('should return a next bonus points from next ball rolled', () => {
-    expect(bowling([{ball1: 5 ,ball2: 5},{ball1: 5 ,ball2: 0},{ball1: 0 ,ball2: 0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0}])).to.equal(15);
+    expect(bowling([{ball1: 5 ,ball2: 5},{ball1: 5 ,ball2: 0},{ball1: 0 ,ball2: 0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0}])).to.equal(20);
   });
   it('should return next bonus points from the next two balls rolled', () => {
-    expect(bowling([{ball1: 10 ,ball2: 0},{ball1: 1 ,ball2: 2},{ball1: 0 ,ball2: 0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0}])).to.equal(13);
+    expect(bowling([{ball1: 10 ,ball2: 0},{ball1: 1 ,ball2: 2},{ball1: 0 ,ball2: 0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0}])).to.equal(16);
   });
   it('should return a extra ball if rolled a strike on ball1 and ball 2 or a spare on ball2', () => {
     expect(bowling([{ball1: 0 ,ball2: 0},{ball1: 0 ,ball2: 0},{ball1: 0 ,ball2: 0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 0 ,ball2:0},{ball1: 10 ,ball2:10 ,ball3:5}])).to.equal(25);
